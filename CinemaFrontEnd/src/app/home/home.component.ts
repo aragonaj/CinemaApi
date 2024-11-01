036dfa9c1a0042efa2bbb9d190860d962476b9e1
ajaragon <julianaragongithub@outlook.es>
2024-11-01 16:28:24 +0100

    Se creó el componente navbar

--- CinemaFrontEnd/src/app/home/home.component.ts
+++ CinemaFrontEnd/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink],
+  imports: [CommonModule, RouterOutlet, RouterLink, NavbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
