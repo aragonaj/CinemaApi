fcd3368f028d0e0b3d5461d7d82a80618f3b8d16
ajaragon <julianaragongithub@outlook.es>
2024-11-13 10:56:22 +0100

    Se agregó una nueva barra de navegación para cuando el usuario este logueado

---
+++ CinemaFrontEnd/src/app/apiHomeNavbar/apiHomeNavbar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-apiHomeNavbar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './apiHomeNavbar.component.html',
+  styleUrl: './apiHomeNavbar.component.css'
+})
+export class ApiHomeNavBarComponent {
+
+}
--- CinemaFrontEnd/src/app/views/apiHub/apiHub.component.ts
+++ CinemaFrontEnd/src/app/views/apiHub/apiHub.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from '../../navbar/navbar.component';
+import { ApiHomeNavBarComponent } from "../../apiHomeNavbar/apiHomeNavbar.component";
 
 @Component({
   selector: 'app-apiHub',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, RouterLink, ApiHomeNavBarComponent],
   templateUrl: './apiHub.component.html',
   styleUrl: './apiHub.component.css'
 })
