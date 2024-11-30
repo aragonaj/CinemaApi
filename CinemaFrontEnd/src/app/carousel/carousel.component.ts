db7e936c5c564efda17960fc94e53a31b5b038e7
ajaragon <julianaragongithub@outlook.es>
2024-11-30 17:55:38 +0100

    Se agregó a la sección del carrusel la funcionalidad de cambiar las imágenes y la información de la película

--- CinemaFrontEnd/src/app/carousel/carousel.component.ts
+++ CinemaFrontEnd/src/app/carousel/carousel.component.ts
@@ -11,16 +11,38 @@ export class CarouselComponent {
   currentIndex: number = 0;
 
   changeNext() {
-    const items = document.querySelectorAll('.slider-title');
+    const items = document.querySelectorAll('.slider-data');
+    const images = document.querySelectorAll('.image-data');
+
     items[this.currentIndex].classList.remove('active');
+    images[this.currentIndex].classList.remove('img-active');
+
     this.currentIndex = (this.currentIndex + 1) % items.length;
+
     items[this.currentIndex].classList.add('active');
+    images[this.currentIndex].classList.add('img-active');
   }
 
   changePrev() {
-    const items = document.querySelectorAll('.slider-title');
+    const items = document.querySelectorAll('.slider-data');
+    const images = document.querySelectorAll('.image-data');
+
     items[this.currentIndex].classList.remove('active');
+    images[this.currentIndex].classList.remove('img-active');
+
     this.currentIndex = (this.currentIndex - 1 + items.length) % items.length;
+
     items[this.currentIndex].classList.add('active');
+    images[this.currentIndex].classList.add('img-active');
+  }
+
+  showImage() {
+    const images = document.querySelectorAll('.image-data');
+
+    images[this.currentIndex].classList.remove('img-active');
+    
+    this.currentIndex = (this.currentIndex + 1 + images.length) % images.length;
+
+    images[this.currentIndex].classList.add('img-active');
   }
 }
